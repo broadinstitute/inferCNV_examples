ae140ef286819b86073e226b5a268675cd5a4e97
Brian Haas <brianjohnhaas@gmail.com>
2018-09-14 15:22:33 -0400

    adding full oligodendro data

---
+++ oligodendroglioma/oligodendroglioma_full/run.R
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+
+library("infercnv")
+
+# create the infercnv object
+infercnv_obj = CreateInfercnvObject(raw_counts_matrix="expression.cp100k.matrix",
+                                    annotations_file="cell_patients_assignments.txt",
+                                    delim="\t",
+                                    gene_order_file="../../__gene_position_data/gencode_v19_gene_pos.txt",
+                                    ref_group_names=c("Microglia/Macrophage","Oligodendrocytes (non-malignant)"))
+
+# perform infercnv operations to reveal cnv signal
+infercnv_obj = infercnv::run(infercnv_obj,
+                             cutoff=1, 
+                             out_dir="output_dir", 
+                             cluster_by_groups=T, 
+                             plot_steps=F,
+                             use_zscores=F,
+                             )
+
+# generate final plot
+plot_cnv(infercnv_obj,
+         out_dir="output_dir", 
+         cluster_by_groups=T,
+         color_safe_pal=FALSE,
+         x.center=1,
+         title="inferCNV",
+         obs_title="Observations (Cells)",
+         ref_title="References (Cells)",
+         output_filename="infercnv")
+
+
