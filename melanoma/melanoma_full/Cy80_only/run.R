bec152806c081d6ccf176c4458ab53141ea213cc
Brian Haas <brianjohnhaas@gmail.com>
2018-11-07 15:23:10 -0500

    add

--- melanoma/melanoma_full/Cy80_only/run.R
+++ melanoma/melanoma_full/Cy80_only/run.R
@@ -5,7 +5,7 @@ library("infercnv")
 
 # create the infercnv object
 infercnv_obj = CreateInfercnvObject(raw_counts_matrix="cy80.matrix",
-                                    annotations_file="cy80.3types.cell_annots.txt",
+                                    annotations_file="cy80.cell_annots.txt",
                                     delim="\t",
                                     gene_order_file="../../../__gene_position_data/gencode_v19_gene_pos.txt",
                                     ref_group_names=c("Endothelial", "NK", "B", "T", "CAF"))
@@ -13,11 +13,10 @@ infercnv_obj = CreateInfercnvObject(raw_counts_matrix="cy80.matrix",
 # perform infercnv operations to reveal cnv signal
 infercnv_obj = infercnv::run(infercnv_obj,
                              cutoff=1, 
-                             out_dir="output_dir.3types", 
-                             cluster_by_groups=T, 
+                             out_dir="output_dir", 
+                             cluster_by_groups=F, 
                              plot_steps=F,
                              include.spike=T,
                              mask_nonDE_genes=T
                              )
-
 
