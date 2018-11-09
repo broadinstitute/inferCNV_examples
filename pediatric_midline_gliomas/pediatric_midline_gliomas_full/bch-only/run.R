d782f526a02e2baa7416a87c35e60db190fe6a97
Brian Haas <brianjohnhaas@gmail.com>
2018-11-09 11:48:35 -0500

    adding bch only

--- pediatric_midline_gliomas/pediatric_midline_gliomas_full/bch-only/run.R
+++ pediatric_midline_gliomas/pediatric_midline_gliomas_full/bch-only/run.R
@@ -1,23 +1,25 @@
 #!/usr/bin/env Rscript
 
 
+if (file.exists("ped_midline_gliomas.counts.p100k.matrix.gz") & ! file.exists("ped_midline_gliomas.counts.p100k.matrix") ) {
+    system("gunzip -c ped_midline_gliomas.counts.p100k.matrix.gz > ped_midline_gliomas.counts.p100k.matrix")
+}
+
 library("infercnv")
 
 # create the infercnv object
-infercnv_obj = CreateInfercnvObject(raw_counts_matrix="../melanoma.full.counts.matrix",
-                                    annotations_file="cy81.cell_annots.txt",
+infercnv_obj = CreateInfercnvObject(raw_counts_matrix="../ped_midline_gliomas.counts.p100k.matrix",
+                                    annotations_file="bch.samples",
                                     delim="\t",
                                     gene_order_file="../../../__gene_position_data/gencode_v19_gene_pos.txt",
-                                    ref_group_names=c("Endothelial", "B", "T"))
+                                    ref_group_names=c("Oligodendrocyte", "Immune cell"))
 
 # perform infercnv operations to reveal cnv signal
 infercnv_obj = infercnv::run(infercnv_obj,
                              cutoff=1, 
                              out_dir="output_dir", 
-                             cluster_by_groups=F, 
+                             cluster_by_groups=T, 
                              plot_steps=F,
                              include.spike=T,
                              mask_nonDE_genes=T
                              )
-
-
