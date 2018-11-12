d8e86b30aaf1a95e783b92376780ae0b5d4a9801
Brian Haas <brianjohnhaas@gmail.com>
2018-11-11 21:34:49 -0500

    add

--- oligodendroglioma/oligodendroglioma_full/4patients/run.test.R
+++ oligodendroglioma/oligodendroglioma_full/4patients/run.test.R
@@ -8,14 +8,15 @@ infercnv_obj = CreateInfercnvObject(raw_counts_matrix="../expression.cp100k.matr
                                     annotations_file="cells.txt",
                                     delim="\t",
                                     gene_order_file="../../../__gene_position_data/gencode_v19_gene_pos.txt",
-                                    ref_group_names=c("Oligodendrocytes (non-malignant)"))
+                                    ref_group_names=c("Oligodendrocytes (non-malignant)"),
+                                    max_cells_per_group=50)
 
 # perform infercnv operations to reveal cnv signal
 infercnv_obj = infercnv::run(infercnv_obj,
                              cutoff=1, 
                              out_dir="output_dir", 
                              cluster_by_groups=T, 
-                             plot_steps=F,
+                             plot_steps=T,
                              include.spike=T,
                              mask_nonDE_genes=T,
                              )
